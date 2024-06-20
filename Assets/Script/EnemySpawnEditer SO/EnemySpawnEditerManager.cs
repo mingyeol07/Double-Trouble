393be6cf5e1ec2c6664b46f20a2c7e7d7a2c5115
Mingyeol Kim <neomingyeol@gmail.com>
2024-06-21 01:27:54 +0900

    Enemy Spawn Editer

---
+++ Assets/Script/EnemySpawnEditer SO/EnemySpawnEditerManager.cs
@@ -0,0 +1,14 @@
+// # Systems
+using System.Collections;
+using System.Collections.Generic;
+
+// # Unity
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySpawnEditerManager : MonoBehaviour
+{
+    [SerializeField] private EnemyTiming timing;
+    [SerializeField] private Toggle stopToggle;
+    [SerializeField] private Button createPattern;
+}
