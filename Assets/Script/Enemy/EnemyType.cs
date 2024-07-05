f1a37d6c1e34d9e5d27c571b6a960003d1f9b64c
김민결 <neomingyeol@gmail.com>
2024-07-05 20:45:27 +0900

    Let's go reset enemy

--- Assets/Script/Enemy/EnemyType.cs
+++ Assets/Script/Enemy/EnemyType.cs
@@ -1,6 +1,5 @@
 public enum EnemyType
 {
-    EnemyA = 0, EnemyB, EnemyC, EnemyD, EnemyBossA,
+    EnemyA, EnemyB, EnemyC, EnemyD, EnemyBossA,
     EnemyE, EnemyF, EnemyG, EnemyH, EnemyBossB,
-    EnemyI, EnemyJ, EnemyK, EnemyL, EnemyBossC,
 }
